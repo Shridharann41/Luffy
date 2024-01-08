405d426ed1ffc16314ef2f01d5243eac389ea89e
Shridharann41 <shridharan41@gmail.com>
2024-01-08 02:55:38 +0000

    Music generation

--- app/(dashboard)/(routes)/music/page.tsx
+++ app/(dashboard)/(routes)/music/page.tsx
@@ -1,149 +1,115 @@
 "use client";
 
-import axios from "axios";
 import * as z from "zod";
-import { Heading } from "@/components/heading";
-import { MessageSquare } from "lucide-react";
+import axios from "axios";
+import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+
 import { useRouter } from "next/navigation";
-import { ChatCompletionRequestMessage} from "openai";
+import { Music, Send } from "lucide-react";
+
+import { Heading } from "@/components/heading";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Loader } from "@/components/loader";
+
+
+
+import { formSchema } from "./constants";
 import { Empty } from "@/components/empty";
 
+const MusicPage = () => {
 
-import {formSchema} from "./constants"
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { useState } from "react";
-import { Loader } from "@/components/loader";
-import { cn } from "@/lib/utils";
-import { UserAvatar } from "@/components/user-avatar";
-import { BotAvatar } from "@/components/bot-avatar";
-
-
-
-
-const ConversationPage = () => {
   const router = useRouter();
-  const [messages, setMessages] = useState<ChatCompletionRequestMessage[]>([]);
+  const [music, setMusic] = useState<string>();
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-        prompt: ""
+      prompt: "",
     }
   });
 
   const isLoading = form.formState.isSubmitting;
 
-  const onSubmit = async  (values: z.infer<typeof formSchema>) => {
-    try{
-      const userMessage: ChatCompletionRequestMessage = {
-        role: "user",
-        content: values.prompt,
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      setMusic(undefined);
 
+      const response = await axios.post('/api/music', values);
+      console.log(response)
 
-      };
-      const newMessages = [...messages, userMessage];
-
-      const response = await axios.post("/api/conversation", {
-        messages: newMessages,
-
-      });
-
-      setMessages((current) => [...current, userMessage, response.data]);
+      setMusic(response.data.audio);
       form.reset();
-    } catch (error: any) {
-      //TODO: Open Pro Modal
-      console.log(error);
-
-    }finally{
+    }finally {
       router.refresh();
-
     }
-  };
+  }
 
-    return (
-        <div>
-            <Heading
-                title="Conversation"
-                description="Our best and most advanced conversation model "
-                icon={MessageSquare} iconColor={"text-violet-500"} bgColor={"bg-violet-500/10"} 
-                />
-
-                <div className="px-4 lg;px-8">
-                  <div>
-                    <Form {...form}>
-                      <form onSubmit={form.handleSubmit(onSubmit)}
-                        className="
-                        rounded-lg
-                        border
-                        w-full
-                        p-4
-                        px-3
-                        md:px-3                        
-                        focus-within:shadow-sm
-                        grid
-                        grid-cols-12
-                        gap-2
-                        
-                        "
-                        >
-
-                          <FormField name="prompt"
-                          render={({field}) => (
-                           <FormItem className="col-span-12 lg:col-span-10 ">
-                            <FormControl className="m-0 p-0">
-                              <Input
-                              className="border-0 outline-none
-                              focus-visible:ring-0
-                              focus-visible:ring-transparent"
-                              disabled={isLoading}
-                              placeholder="try: How do I make authentic Indian tea?"
-                              {...field}
-                              />
-                            </FormControl>
-
-                           </FormItem>
-
-                          )}/>
-                          <Button className="col-span-12
-                          lg:col-span-2 w-full" disabled={isLoading}>
-                          Generate
-                          </Button>
-                      </form>
-                    </Form>
-                  </div>
-
-                  <div className="space-y-4 mt-4">
-          {isLoading && (
-            <div className="p-8 rounded-lg w-full flex items-center justify-center bg-muted">
-              <Loader />
-            </div>
-          )}
-          {messages.length === 0 && !isLoading && (
-            <Empty label="No conversation started." />
-          )}
-          <div className="flex flex-col-reverse gap-y-4">
-            {messages.map((message) => (
-              <div 
-                key={message.content} 
-                className={cn(
-                  "p-8 w-full flex items-start gap-x-8 rounded-lg",
-                  message.role === "user" ? "bg-white border border-black/10" : "bg-muted",
-                )}
-              >
-                {message.role === "user" ? <UserAvatar /> : <BotAvatar />}
-                <p className="text-sm">
-                  {message.content}
-                </p>
-              </div>
-            ))}
+  return ( 
+    <div>
+      <Heading
+        title="Music Generation"
+        description="Turn your prompt into music."
+        icon={Music}
+        iconColor="text-emerald-500"
+        bgColor="bg-emerald-500/10"
+      />
+      <div className="px-4 lg:px-8">
+        <Form {...form}>
+          <form 
+            onSubmit={form.handleSubmit(onSubmit)} 
+            className="
+              rounded-lg 
+              border 
+              w-full 
+              p-4 
+              px-3 
+              md:px-6 
+              focus-within:shadow-sm
+              grid
+              grid-cols-12
+              gap-2
+            "
+          >
+            <FormField
+              name="prompt"
+              render={({ field }) => (
+                <FormItem className="col-span-12 lg:col-span-10">
+                  <FormControl className="m-0 p-0">
+                    <Input
+                      className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                      disabled={isLoading} 
+                      placeholder="Piano solo" 
+                      {...field}
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <Button className="col-span-12 lg:col-span-2 w-full" type="submit" disabled={isLoading} size="icon">
+              Generate
+            </Button>
+          </form>
+        </Form>
+        {isLoading && (
+          <div className="p-20">
+            <Loader />
           </div>
-        </div>
+        )}
+        {!music && !isLoading && (
+          <Empty label="No music generated." />
+        )}
+        {music && (
+          <audio controls className="w-full mt-8">
+            <source src={music} />
+          </audio>
+        )}
       </div>
     </div>
    );
 }
  
-export default ConversationPage;
+export default MusicPage;
